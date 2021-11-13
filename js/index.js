123b54fcf4451f7ffe754e2f2f7bd94078681689
Gilbertyegon <gilbert.yegon@student.moringaschool.com>
2021-11-13 15:04:00 +0300

    add js

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let maleAkanNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kwame']
+
+let femaleAkanNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday']
+
+function akanGenerate (event){
+    event.preventDefault()
+    let date = document.getElementById('date').value;
+
+    let gender = document.getElementById('gender').value;
+
+
+    let dayNumber = new Date(date).getDay()
+    if (gender=='' || date ==''){
+        alert('Error! You have to provide both your gender and date of birth')
+    }
+
+
+    else if (gender=='male'){
+        alert(`You were born on ${daysOfWeek [dayNumber]} and your Akan Name is ${maleAkanNames[dayNumber]}`)}
+
+    else{
+        alert(`You were born on ${daysOfWeek[dayNumber]} and your Akan name is ${femaleAkanNames[dayNumber]}`)
+    }
+}
