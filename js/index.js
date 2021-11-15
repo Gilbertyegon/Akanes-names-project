1b163975b797ca2f99a09171e26172baa091a258
Gilbertyegon <91177249+Gilbertyegon@users.noreply.github.com>
2021-11-15 15:15:02 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,17 +4,6 @@ let femaleAkanNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 
 let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday']
 
-function akanGenerate (event){
-    event.preventDefault()
-    let date = document.getElementById('date').value;
-
-    let gender = document.getElementById('gender').value;
-
-
-    let dayNumber = new Date(date).getDay()
-    if (gender=='' || date ==''){
-        alert('Error! You have to provide both your gender and date of birth')
-    }
 
 
     else if (gender=='male'){
